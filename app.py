0cab01388b7a5a14369d0c7e18d0c87383c9476c
Daniel Kosinski <de.kosinski@gmail.com>
2015-07-27 21:55:42 +0200

    Use array to store user data

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+team = {
+    'Daniel Kosinski':'Lubie placki',
+    'Krzysiek Wisniewski':'Lubie placki',
+    'Marcin Bauer':'Lubie placki'
+}
+
 @app.route("/")
 def main_page():
     return render_template('main.html')
 
 @app.route("/zaloga")
 def team_page():
-    return render_template('zaloga.html')
+    return render_template('zaloga.html', team=team)
 
 if __name__ == "__main__":
     app.run(debug=True)
