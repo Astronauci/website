00cb324462eb15865c466548079e50eb17343e76
Daniel Kosinski <de.kosinski@gmail.com>
2015-08-12 00:09:03 +0200

    Add google map using google map api

---
+++ static/custom.js
@@ -0,0 +1,10 @@
+function initialize() {
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+      center: new google.maps.LatLng(53.423899, 14.560027),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
